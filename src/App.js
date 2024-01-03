5209bdf11a8aaef0902eb4acbb7ea62acf817b3e
tirupathamma <tirupathammakalasani@gmail.com>
2024-01-03 14:02:54 +0000

    loginImage

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from './components/Login'
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <div>
+    <Login />
+  </div>
+)
+
+export default App
---
+++ src/components/Login/index.js
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+import './index.css'
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+          alt="website logo"
+        />
+      </div>
+    )
+  }
+}
+export default Login
